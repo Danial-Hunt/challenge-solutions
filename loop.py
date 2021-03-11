46ca525e9ee19bec9edab0b8c15532fb21c912cd
Danial-Hunt <76631486+Danial-Hunt@users.noreply.github.com>
2021-03-11 10:48:20 +0000

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,8 +1,11 @@
-
-
-
-
-i = int(input("Please Input"))
-while i < 5:
-    print(i ** 2)
-    i += 1
+## Author: Danial Hunt
+# Date: 3/11/2021
+# Description: See attached pdf for problem...
+#
+def main():
+    i = int(input("Please Input"))
+        while i < 5:
+            print(i ** 2)
+            i += 1
+while true:
+    main()
