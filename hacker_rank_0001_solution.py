18613f3a2eb673e2ed07316e5b47b09e28f3fc10
Danial-Hunt <76631486+Danial-Hunt@users.noreply.github.com>
2021-03-12 21:57:23 -0700

    Update hacker_rank_0001_solution.py
    Problem Solved

--- hacker_rank_0001_solution.py
+++ hacker_rank_0001_solution.py
@@ -2,13 +2,11 @@
 # Date: 3/11/2021
 # Description: See attached pdf for problem...
 #
-def main():
-    i = int(input("Please Input"))  # Reads input from user and saves it to variable i
-        while i < 5:                # For all non negative integers i < n, print the square of i.
+if __name__ == '__main__':
+    n = int(raw_input())
+    if 1 <= n <= 20: # Insures that contraints of problem are met.
+        for i in range(0, n): # Uses range within a for loop to generate the range and uses the print function to do the calculations required by problem. 
             print(i ** 2)
-            i += 1
-while true:                         # Loop to keep program initialized
-    main()
-
     
     
+## Problem Solved 3/12/2021
