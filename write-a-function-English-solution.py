b138b0c2b5487d0e7d420a1c433f39154fe4367d
Danial-Hunt <76631486+Danial-Hunt@users.noreply.github.com>
2021-03-16 23:52:10 -0600

    Update write-a-function-English-solution.py

--- write-a-function-English-solution.py
+++ write-a-function-English-solution.py
@@ -2,27 +2,21 @@
 # Date: 3/16/2021
 # Description: Takes a year as an input and outputs whether or not it is a leap year.
 #
+## Author: Daniel Hunt
+# Date: 3/16/2021
+# Description: Takes a year as an input and outputs whether or not it is a leap year.
+#
+year = int(input())
+
 def is_leap(year):
+    
     """
     Description: Calculates whether a year is a leap year or not.
     Input: (int) Year 
     Output: (boolean) Whether it is a leap year or not.
     """
-    
-    leap = False
-    
-    if year % 4 == 0:
-        if year % 100:
-            if year % 400:
-                leap = True
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+                              
+  
 
-            else:
-                leap = False
-        else:
-            leap = False
-    else:
-        leap = False
-    
-    return leap
-
-year = int(input())
+print(is_leap(1992))
